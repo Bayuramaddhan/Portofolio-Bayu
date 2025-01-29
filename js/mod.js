1b857b0812e7ce1f09abbc40794b37173400abdd
Bayu Ramadhan <66245216+Bayuramaddhan@users.noreply.github.com>
2025-01-29 18:58:31 +0700

    update design system
    update design system

---
+++ js/mod.js
@@ -0,0 +1,37 @@
+const freeBtn = document.querySelector("#free-btn");
+const gorBtn = document.querySelector("#gor-btn");
+const freeDesign = document.querySelector(".free-content");
+const goroadDesign = document.querySelector(".goroad-content");
+
+//mengaktifkan button free sejak awal
+activeStart();
+
+function activeStart(){
+    freeBtn.classList.add("active-btn");
+    goroadDesign.classList.add("hidden");
+}
+
+function activefreeBtn(){
+    //cek apakah free btn mempunya class active btn, ! untuk melakukan sebaliknya, melihat freebtn tidak punya class active-btn
+    if (!freeBtn.classList.contains("active-btn")){
+        freeBtn.classList.add("active-btn");
+        gorBtn.classList.remove("active-btn");
+    }
+    acvtiveDesignFree();
+}
+
+function activegorBtn(){
+    freeBtn.classList.remove("active-btn");
+    gorBtn.classList.add("active-btn");
+
+    acvtiveDesignGoroad();
+}
+
+function acvtiveDesignFree(){
+    freeDesign.classList.remove("hidden");
+    goroadDesign.classList.add("hidden");
+}
+function acvtiveDesignGoroad(){
+    freeDesign.classList.add("hidden");
+    goroadDesign.classList.remove("hidden");
+}
